c6424d8dc61c654644ed5c265daa7a8275d78cb9
aaryan10 <aaryanbudhiraja2@gmail.com>
2018-06-26 04:20:57 +0530

    origin

---
+++ main.js
@@ -0,0 +1,69 @@
+// 2048 The Game 
+
+let i;
+let j;
+	
+//Creating Grid
+
+function create2DArray(rows)	{
+
+	grid = [];
+
+	for(i=0; i<rows; i++)	{
+		grid[i]=[];
+	}
+
+	return grid;
+}
+
+create2DArray(4);
+
+for(i=0; i<4; i++)	{
+	for(j=0; j<4; j++)	{
+		grid[i][j] = 0;
+	}
+}
+	
+// Assigning 2 to random positions
+
+grid[Math.floor(4*Math.random())][Math.floor(4*Math.random())] = 2;
+grid[Math.floor(4*Math.random())][Math.floor(4*Math.random())] = 2;
+
+
+//Press left key
+
+document.addEventListener('keyup', leftKey);
+
+function leftKey(e) {
+	if(e.keyCode == 37)	{
+
+		for(i=0; i<4; i++)	{
+			for(j=3; j>=0; j--)	{
+				if(grid[i][j] == 0) {}
+
+				else {
+					for(k=j-1; k>=0; k--)	{
+						if(grid[i][k] == 0){
+							grid[i][k] = grid[i][j];
+							grid[i][j] = 0;
+							j--;
+						}
+
+						else {
+							if(grid[i][k] == grid[i][j]) {
+								grid[i][k] *= 2;
+								grid[i][j] = 0;
+							}
+							else if(grid[i][k] != grid[i][j]){}
+						
+						}
+					}
+				}
+			}
+		}
+		grid[Math.floor(4*Math.random())][Math.floor(4*Math.random())] = 2;	
+	}	
+}
+
+// Display in console
+console.log(grid);
