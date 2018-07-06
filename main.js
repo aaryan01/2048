981114ed5171449f3dac352c6465d18ee93a6f82
aaryan10 <aaryanbudhiraja2@gmail.com>
2018-07-06 06:11:22 +0530

    origin

--- main.js
+++ main.js
@@ -64,7 +64,26 @@ document.addEventListener('keyup', leftKey);
 
 function leftKey(e) {
 	if(e.keyCode == 37)	{
-
+		for(i=0; i<4; i++)	{
+			for(j=1; j<4; j++)	{
+				if(grid[i][j] != 0)	{
+					for(x=j; x>0; x--)	{
+						if(grid[i][x-1] == 0)	{
+							grid[i][x-1] = grid[i][x];
+							grid[i][x] = 0;
+						}
+						else if(grid[i][x] == grid[i][x-1])	{
+							grid[i][x-1] *= 2;
+							grid[i][x] = 0;
+							break;
+						}
+						else break;
+					}
+				}
+			}
+		}		
+	console.log(grid);
+	randomTwo(grid);
 		
 	}	
 }
@@ -76,7 +95,7 @@ document.addEventListener('keyup', rightKey);
 
 function rightKey(e) {
 	if(e.keyCode == 39)	{
-		for(i=0; y<4; i++)	{
+		for(i=0; i<4; i++)	{
 			for(j=2; j>=0; j--)	{
 				if(grid[i][j] != 0)	{
 					for(x=j; x<3; x++)	{
@@ -106,7 +125,26 @@ document.addEventListener('keyup', upKey);
 
 function upKey(e) {
 	if(e.keyCode == 38)	{
-
+		for(j=0; j<4; j++)	{
+			for(i=1; i<4; i++)	{
+				if(grid[i][j] != 0)	{
+					for(x=i; x>0; x--)	{
+						if(grid[x-1][j] == 0)	{
+							grid[x-1][j] = grid[x][j];
+							grid[x][j] = 0;
+						}
+						else if(grid[x][j] == grid[x-1][j])	{
+							grid[x-1][j] *= 2;
+							grid[x][j] = 0;
+							break;
+						}
+						else break;
+					}
+				}
+			}
+		}		
+	console.log(grid);
+	randomTwo(grid);
 		
 	}	
 }
@@ -117,7 +155,26 @@ document.addEventListener('keyup', downKey);
 
 function downKey(e) {
 	if(e.keyCode == 40)	{
-
+		for(j=0; j<4; j++)	{
+			for(i=2; i>=0; i--)	{
+				if(grid[i][j] != 0)	{
+					for(x=i; x<3; x++)	{
+						if(grid[x+1][j] == 0)	{
+							grid[x+1][j] = grid[x][j];
+							grid[x][j] = 0;
+						}
+						else if(grid[i][x] == grid[i][x+1])	{
+							grid[i][x+1] *= 2;
+							grid[i][x] = 0;
+							break;
+						}
+						else break;
+					}
+				}
+			}
+		}		
+	console.log(grid);
+	randomTwo(grid);
 		
 	}	
 }
