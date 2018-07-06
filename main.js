f2049e2caeebc8b565bc6bceb0193594a5e40120
aaryan10 <aaryanbudhiraja2@gmail.com>
2018-07-06 18:30:33 +0530

    origin

--- main.js
+++ main.js
@@ -177,6 +177,42 @@ function downKey(e) {
 	}	
 }
 
+// Display
+
+if(grid[0][0] !=0 ) 
+	document.getElementsByClassName("tile-1")[0].innerHTML = grid[0][0];
+if(grid[0][1] !=0 ) 
+	document.getElementsByClassName("tile-2")[0].innerHTML = grid[0][1];
+if(grid[0][2] !=0 ) 
+	document.getElementsByClassName("tile-3")[0].innerHTML = grid[0][2];
+if(grid[0][3] !=0 ) 
+	document.getElementsByClassName("tile-4")[0].innerHTML = grid[0][3];
+if(grid[1][0] !=0 ) 
+	document.getElementsByClassName("tile-5")[0].innerHTML = grid[1][0];
+if(grid[1][1] !=0 ) 
+	document.getElementsByClassName("tile-6")[0].innerHTML = grid[1][1];
+if(grid[1][2] !=0 ) 
+	document.getElementsByClassName("tile-7")[0].innerHTML = grid[1][2];
+if(grid[1][3] !=0 ) 
+	document.getElementsByClassName("tile-8")[0].innerHTML = grid[1][3];
+if(grid[2][0] !=0 ) 
+	document.getElementsByClassName("tile-9")[0].innerHTML = grid[2][0];
+if(grid[2][1] !=0 ) 
+	document.getElementsByClassName("tile-10")[0].innerHTML = grid[2][1];
+if(grid[2][2] !=0 ) 
+	document.getElementsByClassName("tile-11")[0].innerHTML = grid[2][2];
+if(grid[2][3] !=0 ) 
+	document.getElementsByClassName("tile-12")[0].innerHTML = grid[2][3];
+if(grid[3][0] !=0 ) 
+	document.getElementsByClassName("tile-13")[0].innerHTML = grid[3][0];
+if(grid[3][1] !=0 ) 
+	document.getElementsByClassName("tile-14")[0].innerHTML = grid[3][1];
+if(grid[3][2] !=0 ) 
+	document.getElementsByClassName("tile-15")[0].innerHTML = grid[3][2];
+if(grid[3][3] !=0 ) 
+	document.getElementsByClassName("tile-16")[0].innerHTML = grid[3][3];
+
+
 
 
 
